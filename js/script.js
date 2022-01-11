07b1bccc32e968b885a5a4f3794b1c86bd686846
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-11 13:46:43 -0600

    feat: GET data from external API using fetch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click', getExternalData);
 //For local text file
 function getText() {
     fetch('text.txt')
@@ -30,6 +32,25 @@ function getJson() {
             data.forEach(function(post) {
                 output += `<li>${post.title}</li>
                             <li>${post.body}</li>`;
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+//For API data
+function getExternalData() {
+    fetch('https://api.github.com/users')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            let output = '';
+            data.forEach(function(user) {
+                output += `<li>${user.login}</li>`;
             });
             document.getElementById('output').innerHTML = output;
         })
