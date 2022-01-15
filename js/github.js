6a1dca0bc7ab079ab2a14c94afc0cf3e4242de2f
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-15 10:51:33 -0600

    feat: established endpoint to showcase users latest repos

--- js/github.js
+++ js/github.js
@@ -2,15 +2,22 @@ class Github {
     constructor() {
         this.CLIENT_ID = CLIENT_ID;
         this.CLIENT_SECRET = CLIENT_SECRET;
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`);
+
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
