90c690335d95c476a2886501440de3a23fcd984c
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-15 14:28:14 -0600

    feat: REGEX metacharacter eval pt2 regex

--- js/regular-expressions.js
+++ js/regular-expressions.js
@@ -1,12 +1,13 @@
 "use strict"
 
-let re;
-re = /hello/i; //i = case insensitive
+// let re;
+// re = /hello/i; //i = case insensitive
 // re = /hello/g; //g = Global search
 
-console.log(re);
-console.log(re.source);
+// console.log(re);
+// console.log(re.source);
 
+/////////////===============   REGEX EVALUATION FUNCTIONS
 
 //exec() - Return result in an array
 // const result = re.exec('Hello world');
@@ -27,6 +28,43 @@ console.log(re.source);
 // console.log(result);
 
 //replace() - Returns new string with some or all matches of a pattern
-const str = 'Hello There';
-const newStr = str.replace(re, 'Hi');
-console.log(newStr);
+// const str = 'Hello There';
+// const newStr = str.replace(re, 'Hi');
+// console.log(newStr);
+
+
+/////////////=================  REGEX METACHARACTER SYMBOLS
+
+let re;
+//Literal Characters
+re = /hello/;
+re = /hello/i;
+
+//Meta Character symbols
+re = /^h/i;    // === MUST START WITH
+re = /d$/i;    // === MUST END WITH
+re = /^hello$/i;    // === MUST END/BEGIN WITH
+re = /^h.llo$/i;    // === MATCHES ANY ONE CHAR
+re = /h*llo/i;    // === MATCHES ANY CHAR 0 OR MORE TIMES
+re = /gre?a?y/i   // === OPTIONAL CHAR
+re = /gre?a?y\?/i   // === ESCAPE CHAR
+
+
+
+
+//STRING TO MATCH
+const str = 'Grey?';
+
+//LOG RESULTS
+const result = re.exec(str);
+console.log(result);
+
+function regTest(re, str) {
+    if(re.test(str)) {
+        console.log(`${str} matches ${re.source}`);
+    } else {
+        console.log(`${str} DOES NOT match${re.source}`)
+    }
+}
+
+regTest(re, str);
